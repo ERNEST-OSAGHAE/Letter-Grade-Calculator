ae251d9e9610742564729a8d53d2c9757eba5f5e
Ernest Osaghae <43833146+ERNEST-OSAGHAE@users.noreply.github.com>
2019-07-25 13:18:13 -0500

    Update and rename README.md to GPAcalculator.py

---
+++ GPAcalculator.py
@@ -0,0 +1,41 @@
+# Letter-Grade-Calculator using python.
+This program I created calculates and output your letter grade when you input your GPA.
+This program was created using python.
+
+def main():
+
+    schoolgrade = input("Enter your GPA: ")
+    if schoolgrade >= "3.8":
+        print('Gradel = A+')
+    elif schoolgrade >= "3.4":
+        print('Grade = A-')
+    elif schoolgrade >= "3.1":
+        print('Grade = B+')
+    elif schoolgrade >= "2.8":
+        print('Grade = B')
+    elif schoolgrade >= "2.4":
+        print('Grade = B-')
+    elif schoolgrade >= "2.1":
+        print('Grade = C+')
+    elif schoolgrade >= "1.8":
+        print('Grade = C')
+    elif schoolgrade >= "1.4":
+        print('Grade = C-')
+    elif schoolgrade >= "1.1":
+        print('Grade = D+')
+    elif schoolgrade >= "0.6":
+        print('Grade = D')
+    elif schoolgrade >= "0.0":
+        print('Grade = F')
+    if schoolgrade >= "":
+        main()
+
+    else:
+        exit()
+
+main()
+
+
+
+# if client enters blank, echo "enter your GPA
+# if client enter non int, echo "enter numbers only"
--- README.md
+++
@@ -1,4 +0,0 @@
-# Letter-Grade-Calculator using python.
-This program I created calculates and output your letter grade when you input your GPA.
-This program was created using python.
-
